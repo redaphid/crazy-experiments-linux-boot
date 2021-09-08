899d6eae841566c0d6e34d6eeab2016689461c37
redaphid <iam@aaronherres.com>
2021-09-07 19:16:22 -0700

    binding the scripts in

--- scripts/grub-rescue-build.fish
+++ scripts/grub-rescue-build.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 docker build --tag crazy-experiments-linux:builder -f ./docker/builder.Dockerfile .; or exit 1
 docker run \
-    --mount type=bind,source=$PWD/docker,target=/project/docker \
+    --mount type=bind,source=$PWD/scripts,target=/project/scripts \
     -it crazy-experiments-linux:builder /usr/bin/fish    
