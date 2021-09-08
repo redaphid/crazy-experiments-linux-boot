ff87eb96986ef0739b5c6025dd5fec351f5f0ca0
redaphid <iam@aaronherres.com>
2021-09-07 19:19:33 -0700

    testing script execution

--- scripts/grub-rescue-build.fish
+++ scripts/grub-rescue-build.fish
@@ -2,4 +2,5 @@
 docker build --tag crazy-experiments-linux:builder -f ./docker/builder.Dockerfile .; or exit 1
 docker run \
     --mount type=bind,source=$PWD/scripts,target=/project/scripts \
-    -it crazy-experiments-linux:builder /usr/bin/fish    
+    --mount type=bind,source=$PWD/tmp,target=/project/tmp \
+    -it crazy-experiments-linux:builder /project/scripts/hello-world.fish   
--- scripts/test.fish
+++ scripts/test.fish
@@ -1,3 +1,2 @@
 #!/usr/bin/env fish
-echo "hey there"
 qemu-system-x86_64 -hda ./tmp/debian.img -m 2048 -smp cpus=4
