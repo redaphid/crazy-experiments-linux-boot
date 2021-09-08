527f560f6359cfcfea5ef3859d9457cafdee747a
redaphid <iam@aaronherres.com>
2021-09-07 19:15:02 -0700

    got them volume mounts binding

--- scripts/grub-rescue-build.fish
+++ scripts/grub-rescue-build.fish
@@ -1,3 +1,5 @@
 #!/usr/bin/env fish
 docker build --tag crazy-experiments-linux:builder -f ./docker/builder.Dockerfile .; or exit 1
-docker run -it crazy-experiments-linux:builder /usr/bin/fish
+docker run \
+    --mount type=bind,source=$PWD/docker,target=/project/docker \
+    -it crazy-experiments-linux:builder /usr/bin/fish    
