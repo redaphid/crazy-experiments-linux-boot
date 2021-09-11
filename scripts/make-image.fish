59f49995e41ca72f2de6d95ed202e481002e2623
redaphid <iam@aaronherres.com>
2021-09-10 22:54:22 -0700

    create a working efi partition! that does nothing.

--- scripts/make-image.fish
+++ scripts/make-image.fish
@@ -1,3 +1,8 @@
 #!/usr/bin/env fish
-grub-mkrescue -o ./tmp/rescue.img
-truncate --size 4G ./tmp/rescue.img
+set -l DISK ./tmp/os.img
+rm os.img
+grub-mkrescue -o $DISK
+truncate --size 4G $DISK
+sgdisk $DISK -s
+printf "fix\n" | parted ---pretend-input-tty $DISK print
+parted --script $DISK rm 3 rm 4 resizepart 2 100%
--- scripts/mount-image.fish
+++ scripts/mount-image.fish
@@ -3,5 +3,5 @@ set -l TMP '/project/tmp'
 mkdir -p ./tmp/mnt/os
 losetup -D
 set IMG_LOOP (losetup -f)
-losetup -o (expr 512 \* 2048) $IMG_LOOP  ./tmp/debian.img
+losetup -o (expr 512 \* 204) $IMG_LOOP  ./tmp/os.img
 mount -t auto $IMG_LOOP ./tmp/mnt/os
