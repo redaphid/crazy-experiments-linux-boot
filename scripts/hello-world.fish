82856ecedffd8bf3ec96191e7dd9d228c1918f0b
redaphid <iam@aaronherres.com>
2021-09-10 20:36:24 -0700

    making sure I can write to /tmp

--- scripts/hello-world.fish
+++ scripts/hello-world.fish
@@ -1,2 +1,4 @@
 #!/usr/bin/env fish
 echo 'Hello, World!'
+touch /project/tmp/hi
+ls /project/tmp
--- scripts/run.fish
+++ scripts/run.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 docker build --tag crazy-experiments-linux:builder -f ./docker/builder.Dockerfile .; or exit 1
+echo $PWD
 docker run \
     --mount type=bind,source=$PWD/scripts,target=/project/scripts \
     --mount type=bind,source=$PWD/tmp,target=/project/tmp \
